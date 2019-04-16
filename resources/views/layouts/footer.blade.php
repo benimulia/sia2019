4a6fbe1cb43e73af9696e7455df4f6a0d1f4874f
benimulia <beni.mulia@si.ukdw.ac.id>
2019-04-16 15:24:24 +0700

    udah jadi gan:v

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,9 @@
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+      :)
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2019 - Didimus Candra & Beni Mulia <br><a href="#">Sistem Informasi Akuntansi</a>.</strong> All rights reserved.
+  </footer>
